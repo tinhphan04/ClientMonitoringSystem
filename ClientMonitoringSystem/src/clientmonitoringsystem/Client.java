8ed65ea63e48c9ee4d34a7e67b835d629f44c243
Tinh Phan Van <tinh.pv@cep.org.vn>
2022-07-24 11:01:21 +0700

    24/07/2022 - Work with Multiple Client

---
+++ ClientMonitoringSystem/src/clientmonitoringsystem/Client.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clientmonitoringsystem;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ *
+ * @author tinh.pv
+ */
+public class Client {
+    public final static String SERVER_IP = "127.0.0.1";
+    public final static int SERVER_PORT = 7000;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = null;
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT); // Connect to server
+            System.out.println("Connected: " + socket);
+ 
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+            for (int i = '0'; i <= '9'; i++) {
+                os.write(i); // Send each number to the server
+                int ch = is.read(); // Waiting for results from server
+                System.out.print((char) ch + " "); // Display the results received from the server
+                Thread.sleep(200);
+            }
+        } catch (IOException ie) {
+            System.out.println("Can't connect to server");
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
+
